18100f8ef956ee04305b360ada18d8a981adaeea
Alejandro3009 <>
2022-03-19 19:43:50 +0000

    new try

--- list.c
+++ list.c
@@ -37,14 +37,17 @@ List * createList() {
 }
 
 void * firstList(List * list) {
-  if(list->head == NULL)return NULL;
-  else list->current = list->head;
+  if(list->current == NULL)return NULL;
+  
+  list->current = list->head;
   return list->current->data;
 }
 
 void * nextList(List * list) {
   if(list->current->next == NULL)return NULL;
-  else list->current = list->current->next;
+  if(list->current == NULL)return NULL;
+  
+  list->current = list->current->next;
   return list->current->data;
 }
 
